30f9347aee9960ad4d85e13ed7194df79bf42e9c
PratikRangari <rangaripratik1998@gmail.com>
2024-02-08 16:20:15 +0530

    some change footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -7,11 +7,11 @@ export default function Footer() {
 
       <div className='row'>
 
-        <div className='col-md-6 col-sm-12 mb-3 mb-sm-0'>
+        <div className='col-md-6 col-sm-12 mb-3 mb-sm-0 footer-sml'>
           <h6 className='mb-0 mt-2'>Copyright © 2020 <Link to="/">PuffinTheme.</Link></h6>
         </div>
 
-        <div className='col-md-6 col-sm-12 d-flex  justify-content-end'>
+        <div className='col-md-6 col-sm-12 d-flex  justify-content-end footer-sml'>
           <h6 className='mb-2 mx-3 mt-2'>Documentation FAQ</h6>
           <button type="button" className="btn btn-outline-secondary btn-sm mx-0">Buy Now</button>
         </div>
