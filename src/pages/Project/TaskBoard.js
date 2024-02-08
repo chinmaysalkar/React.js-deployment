8cadf4e59b28be7370947b0a257cf419d1efaa1f
PratikRangari <rangaripratik1998@gmail.com>
2024-02-08 16:48:25 +0530

    c

--- src/pages/Project/TaskBoard.js
+++ src/pages/Project/TaskBoard.js
@@ -17,10 +17,7 @@ export default function TaskBoard() {
       setActiveButton(componentName);
   };
   
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  
 
 
     const progressData = [
