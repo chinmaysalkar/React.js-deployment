d6ca0fd13610c1627acc464d5f6a90ff61c9ac73
PratikRangari <rangaripratik1998@gmail.com>
2024-02-12 14:47:06 +0530

    For Delete Function add

--- src/components/Department/Gridlist.js
+++ src/components/Department/Gridlist.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash,faPencil } from '@fortawesome/free-solid-svg-icons';
 import avatar1 from '../../assets/images/avatar1.jpg'
@@ -6,7 +6,7 @@ import avatar2 from '../../assets/images/avatar2.jpg'
 import avatar3 from '../../assets/images/avatar3.jpg'
 
 export default function Gridlist() {
-    const employees = [
+    const [employees, setEmployees] = useState([
         { name: 'John Smith', role: 'Web Development', employeeCount: 105, earnings: '$3100', imageSrc:  avatar1 },
         { name: 'Maryam Amiri', role: 'Web Development', employeeCount: 105, earnings: '$3100', imageSrc: avatar2},
         { name: 'Fidel Tonn ', role: 'Web Development', employeeCount: 12, earnings: '$3100', imageSrc: avatar1 },
@@ -15,7 +15,22 @@ export default function Gridlist() {
         { name: 'John Smith ', role: 'Web Development', employeeCount: 55, earnings: '$12,045', imageSrc: avatar2 },
         { name: 'John Smith ', role: 'Web Development', employeeCount: 23, earnings: '$5100', imageSrc: avatar3 },
         
-      ];
+    ]);
+
+      const handleEdit = (index) => {
+        
+        console.log('Edit employee:', index);
+    };
+
+
+      const handleDelete = (index) => {
+        
+        setEmployees((prevEmployees) => {
+            const updatedEmployees = [...prevEmployees];
+            updatedEmployees.splice(index, 1);
+            return updatedEmployees;
+        });
+    };
   return (
     <div>
         <div className='mt-4 mx-3'>
@@ -27,14 +42,14 @@ export default function Gridlist() {
                         <img className="img-thumbnail rounded-circle avatar-xxl" src={employee.imageSrc} alt={employee.name} />
                         <h6 className="mt-3">{employee.name}</h6>
                         <div className="text-center text-muted mb-3">{employee.role}</div>
-                        <button type="button" className="btn btn-icon btn-outline-primary mx-1">
+                        <button type="button" className="btn btn-icon btn-outline-primary mx-1" onClick={() => handleEdit(index)}>
                         <FontAwesomeIcon icon={faPencil} />
                         </button>
-                        <button type="button" className="btn btn-icon btn-outline-danger">
+                        <button type="button" className="btn btn-icon btn-outline-danger" onClick={() => handleDelete(index)}>
                         <FontAwesomeIcon icon={faTrash} />
                         </button>
                     </div>
-                    <div className="card-footer text-center">
+                    <div className="card-footer text-center bg-white">
                         <div className="row clearfix">
                             <div className="col-6">
                             <h5 className="mb-0">{employee.employeeCount}</h5>
